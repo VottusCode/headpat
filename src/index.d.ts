4e4e8e0f2527280309d6f03d399e1d7001ac50ff
Mia <vottus@vott.us>
2021-06-19 19:40:25 +0200

    feat: init

---
+++ src/index.d.ts
@@ -0,0 +1,5 @@
+class Headpat {
+  pat(): string;
+}
+
+export default Headpat;
---
+++ src/index.js
@@ -0,0 +1,7 @@
+class Headpat {
+  pat() {
+    return "Espi is cute. Everyone be kind to @sysdotini, a lil cute girl.";
+  }
+}
+
+module.exports = Headpat;
